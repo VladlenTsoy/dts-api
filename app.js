98e4295a7f0366daa765c7b353e47f90651a08bf
vladlen <vladlengg@gmail.com>
2022-03-01 00:02:17 +0500

    update

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express")
-const path = require("path")
-const cookieParser = require("cookie-parser")
-const logger = require("morgan")
-
 const indexRouter = require("./routes/index")
 
 const app = express()
 
-app.use(logger("dev"))
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "public")))
+// app.use(express.json())
+// app.use(express.urlencoded({extended: false}))
+// app.use(cookieParser())
+// app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/", indexRouter)
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
 
 module.exports = app
