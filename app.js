2af25476ed8567789335f755d2ed890f8457bfc5
vladlen <vladlengg@gmail.com>
2022-03-01 00:08:34 +0500

    update

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express")
 const indexRouter = require("./routes/index")
 
 const app = express()
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3000
 //
+app.use(express.json({extended: false}))
 app.use("/", indexRouter)
 //
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
-});
+    console.log(`Server started at http://localhost:${port}`)
+})
 
 module.exports = app
