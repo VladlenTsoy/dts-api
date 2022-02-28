88467dbf558e821231fb2d2113a275bf33ab7a28
vladlen <vladlengg@gmail.com>
2022-03-01 00:03:43 +0500

    update

--- app.js
+++ app.js
@@ -3,13 +3,8 @@ const indexRouter = require("./routes/index")
 
 const app = express()
 
-// app.use(express.json())
-// app.use(express.urlencoded({extended: false}))
-// app.use(cookieParser())
-// app.use(express.static(path.join(__dirname, "public")))
-
 app.use("/", indexRouter)
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3030;
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
