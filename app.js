f0f3ea63c35255e9729fadf18397e6d929581937
vladlen <vladlengg@gmail.com>
2022-03-01 00:12:57 +0500

    update

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const indexRouter = require("./routes/index")
+const helmet = require("helmet")
 
 const app = express()
 const port = process.env.PORT || 3000
 //
+app.use(helmet())
 app.use(express.json({extended: false}))
 app.use("/", indexRouter)
 //
